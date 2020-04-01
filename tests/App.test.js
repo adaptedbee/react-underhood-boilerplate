ad50fc28b7325b2d9d4456e5b1a6baf27d775a71
Mikhail Savotikov <adaptedbee@yandex.ru>
2020-04-01 05:08:32 +0300

    Обновляем тест

--- tests/App.test.js
+++ tests/App.test.js
@@ -2,16 +2,18 @@ import App from "../samples/App";
 
 test("jsx works", () => {
   expect(App).toEqual({
-    "props": {
-      "children": [{
-        "props": {
-          "children": [], 
-          "nodeValue": "Hello, World!"
-        }, 
-        "type": "TEXT ELEMENT"
-      }], 
-      "prop1": "prop value"
-    }, 
-    "type": "h1"
+    props: {
+      children: [
+        {
+          props: {
+            children: [],
+            nodeValue: "Hello, World!"
+          },
+          type: "TEXT ELEMENT"
+        }
+      ],
+      prop1: "prop value"
+    },
+    type: "h1"
   });
 });
