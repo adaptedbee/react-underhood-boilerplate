79187f8b0fe1c9e2903fcfdb8fc81334fb03ba24
Mikhail Savotikov <adaptedbee@yandex.ru>
2020-05-07 15:26:44 +0300

    Заменяем ListItem на функциональный компонент

--- samples/ListItem.jsx
+++ samples/ListItem.jsx
@@ -1,13 +1,5 @@
 import OwnReact from "../src";
-import Component from "../src/Component";
 
-// eslint-disable-next-line react/prefer-stateless-function
-class ListItem extends Component {
-  render() {
-    const { children } = this.props;
-
-    return <div>{children}</div>;
-  }
-}
+const ListItem = ({ children }) => <div>{children}</div>;
 
 export default ListItem;
