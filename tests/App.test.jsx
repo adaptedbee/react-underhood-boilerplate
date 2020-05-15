fef9648dfb54373fd0d86c21d9bd57564c08a499
Mikhail Savotikov <adaptedbee@yandex.ru>
2020-05-15 13:18:25 +0300

    Тест для функции сортировки

--- tests/App.test.jsx
+++ tests/App.test.jsx
@@ -1,7 +1,7 @@
 import OwnReact from "../src";
+import { sortAlphabetByString } from "../samples/utils";
 
 const TestApp = <h1 prop1="prop value">Hello, World!</h1>;
-
 test("jsx works", () => {
   expect(TestApp).toEqual({
     props: {
@@ -18,4 +18,12 @@ test("jsx works", () => {
     },
     type: "h1"
   });
+});
+
+test("alphabet sorting works", () => {
+  const alphabetArray = ["А", "Б", "В", "Г", "Д"];
+  const sortString = "ГБА";
+  const finalArray = ["Г", "Б", "В", "А", "Д"];
+
+  expect(sortAlphabetByString(alphabetArray, sortString)).toEqual(finalArray);
 });
