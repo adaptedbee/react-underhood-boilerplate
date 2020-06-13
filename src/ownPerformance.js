59f40227e73f5026008e5c500a6bbf446ed888ef
Mikhail Savotikov <adaptedbee@yandex.ru>
2020-06-13 16:50:03 +0300

    Добавляем подсчёт статистики

--- src/ownPerformance.js
+++ src/ownPerformance.js
@@ -1,6 +1,11 @@
 const { performance } = window;
 
 const ownPerformance = {
+  statistics: {
+    wrongRenderCounter: 0,
+    totalTime: {}
+  },
+
   start(markName) {
     performance.mark(`${markName} start`);
   },
@@ -16,8 +21,14 @@ const ownPerformance = {
       `${markName} end`
     );
 
-    return duration;
+    if (!this.statistics.totalTime[markName]) {
+      this.statistics.totalTime[markName] = duration;
+    } else {
+      this.statistics.totalTime[markName] += duration;
+    }
   }
 };
+
+window.performance_OwnReact = ownPerformance.statistics;
 
 export default ownPerformance;
